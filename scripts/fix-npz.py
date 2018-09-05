6585265f73df48cc58da2f24d007d92ccb9da388
lg <lg4869@outlook.com>
2018-09-06 07:31:36 +0800

    a tool for renaming tensors

---
+++ scripts/fix-npz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+
+import numpy as np
+
+filename = os.path.join(os.getenv('HOME'), 'Downloads/vgg450000.npz')
+new_filename = os.path.join(os.getenv('HOME'), 'Downloads/vgg450000_no_cpm.npz')
+
+data = np.load(filename)
+
+new_data = dict()
+for name, t in data.items():
+    print('%s' % (name))
+    new_name = name.replace('cpm/', '')
+    new_data[new_name] = t
+
+np.save(new_filename, new_data)
